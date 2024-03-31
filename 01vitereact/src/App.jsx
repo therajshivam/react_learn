0ae5227bbe26495bf16cdd107867d656c9c5c9ed
therajshivam <rajshivam2386@gmail.com>
2024-03-31 10:30:23 +0530

    vite react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from "./Chai.jsx" 
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>  // fragmentation
+  ) 
+}
+
+export default App
